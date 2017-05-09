a65748076e341622f8851f226a7419be323e00c6
tmendes <tmendes@consensusgroup.net>
2017-05-09 10:55:41 -0300

    up

--- src/main/java/practica/jasper/reports/ReportLog.java
+++ src/main/java/practica/jasper/reports/ReportLog.java
@@ -18,17 +18,19 @@ import practica.jasper.backend.model.LogAuditoria;
 public class ReportLog {
 	
 	public FileInputStream getReport(List<LogAuditoria> datasource) throws JRException, IOException{
-	  String jasperReport = "C:/Users/tribeiro/workspace/jasper-reports/src/main/webapp/jasper/reportLog.jasper";
-	  
+	  //String jasperReport = "C:/Users/tribeiro/workspace/jasper-reports/src/main/webapp/jasper/reportLog.jasper";
+		String jasperReport = "C:/Users/PD/workspace/jasper/reportLog.jasper";
 	  Map<String, Object> params = new HashMap<String, Object>();
-      params.put("TITULO", "LOG OPERACIONES PDOE");
+      params.put("TITULO", "ESTE ES MI TITULO");
+      params.put("USER", "TOMAS");
       
       JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, params, new JRBeanCollectionDataSource(datasource));
       
       File pdf = File.createTempFile("output.", ".pdf");
       JasperExportManager.exportReportToPdfStream(jasperPrint, new FileOutputStream(pdf));
-      JasperExportManager.exportReportToPdfFile(jasperPrint, "C:/Users/tribeiro/workspace/jasper-reports/src/main/webapp/jasper/reportes/ReportLog.pdf");
+     // JasperExportManager.exportReportToPdfFile(jasperPrint, "C:/Users/tribeiro/workspace/jasper-reports/src/main/webapp/jasper/reportes/ReportLog.pdf");
       //JasperExportManager.exportReportToPdfFile(jasperPrint, "C:/Users/PD/workspace/jasper-reports/src/main/webapp/jasper/reportes/ReportLog.pdf");
+      JasperExportManager.exportReportToPdfFile(jasperPrint,"C:/Users/PD/workspace/jasper/reportes/ReportLog.pdf");
       
       return new FileInputStream(pdf);
 	}
